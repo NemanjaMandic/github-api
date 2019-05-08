2ebefbbe16359374ebbafd11f1c7a76e3771c4b1
NemanjaMandic <nemanjans84@gmail.com>
2019-05-08 20:12:33 +0200

    filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar';
 class App extends Component {
@@ -32,8 +31,33 @@ class App extends Component {
       // Catch any errors we hit and update the app
       .catch(error => this.setState({ error, isLoading: false }));
   }
-  onSearchSubmit = (term) => {
-    
+
+  onInputChange = (event) => {
+     console.log(event.target.value);
+     this.setState({
+         term: event.target.value
+     })
+ }
+
+  filterResults = (arr, query) => {
+    return arr.filter(user => user.login.startsWith(query));
+  }
+  onSearchSubmit = (event) => {
+    event.preventDefault();
+    const { term, users} = this.state
+    console.log(term)
+    console.log('DIK', users.filter(u => u.login.startsWith(term)))
+    const filteredUsers = this.filterResults(users, term);
+
+    console.log('FILTERED', filteredUsers);
+    if(term === ''){
+      this.setState({
+        users
+      })
+    }
+    this.setState({
+      users: [...filteredUsers]
+    })
     
   }
 
@@ -44,13 +68,13 @@ class App extends Component {
    
     return (
       <div className="App ui container">
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit} onInputChange={this.onInputChange} />
         
          {users.map(user => {
-           const followers = user.followers_url;
+           
            return(
            
-             <div style={{border: "1px solid black", marginBottom: "20px"}}>
+             <div style={{border: "1px solid black", marginBottom: "20px"}} key={user.id}>
                 <a href={user.html_url}>
                <p>{user.login}</p>
                <img src={user.avatar_url} alt={user.login} />
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,35 +1,26 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
+const SearchBar = ({onInputChange, onSubmit, term}) => {
     
-    state = {
-        term: ''
-    }
-    onInputChange = (event) => {
-       // console.log(event.target.value);
-        this.setState({
-            term: event.target.value
-        })
-    }
 
-    onFormSubmit = (event) => {
-        event.preventDefault();
-        this.props.onSubmit(this.state.term);
-    }
-    render() { 
+
+    // onFormSubmit = (event) => {
+    //     event.preventDefault();
+    //     this.props.onSubmit(this.state.term);
+    // }
+  
         return ( 
             <div className="ui segment">
-            <h1>{this.state.term}</h1>
-             <form onSubmit={this.onFormSubmit} className="ui form">
+             <form onSubmit={onSubmit} className="ui form">
              <h3>Ovde Logo</h3>
                  <div className="field">
                  
-                 <input type="text" value={this.state.term} placeholder="Search" onChange={ this.onInputChange }/>
+                 <input type="text" value={term} placeholder="Search" onChange={ onInputChange }/>
                  </div>
              </form>
             </div>
          );
-    }
+    
 }
 
  
